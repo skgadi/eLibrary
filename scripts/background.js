beeb15bcf343de23e9be072ef583fb1e73d934e4
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2016-08-11 18:30:26 -0500

    001 frameless window achieved

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,11 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('scripts/index.html', {
+  	'frame': 'none',
+  	'id': 'mainWind',
     'outerBounds': {
       'width': 400,
       'height': 500
-    }
+    },
+    'state': 'maximized'
   });
 });
